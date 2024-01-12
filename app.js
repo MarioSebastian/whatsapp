59cf8d2d7f8c79b6848bb44daba3391dd24739df
mariosebastian <mariosebastian@owleyewear.id>
2024-01-12 22:22:56 +0700

    - fixed sending to da groups

--- app.js
+++ app.js
@@ -14,22 +14,21 @@ const whatsapp = require("./z_functions/whatsapp/whatsapp");
 app.post("/sendWhatsappNumber", (req, res) => {
   whatsapp.sendWhatsappNumber(req.body.phoneNumber, req.body.message, res);
 });
-app.post("/sendWhatsappMassNumber", (req, res) => {
-    whatsapp.sendWhatsappMassNumber(req.body.phoneNumberList, req.body.message, res)
-});
 app.post("/sendWhatsappGroupMessage", (req, res) => {
-  res.send(
-    whatsapp.sendWhatsappGroupMessage(req.body.groupName, req.body.message)
-  );
+  whatsapp.sendWhatsappGroupMessage(req.body.groupName, req.body.message);
 });
-app.post("/sendWhatsappMassGroupMessage", (req, res) => {
-  res.send(
-    whatsapp.sendWhatsappMassGroupMessage(
-      req.body.groupNameList,
-      req.body.message
-    )
-  );
-});
-app.get("/test", (req, res) => {
-  res.send("server ok");
-});
+
+// app.post("/sendWhatsappMassNumber", (req, res) => {
+//   whatsapp.sendWhatsappMassNumber(req.body.phoneNumberList, req.body.message, res)
+// });
+// app.post("/sendWhatsappMassGroupMessage", (req, res) => {
+//   res.send(
+//     whatsapp.sendWhatsappMassGroupMessage(
+//       req.body.groupNameList,
+//       req.body.message
+//     )
+//   );
+// });
+// app.get("/test", (req, res) => {
+//   res.send("server ok");
+// });
