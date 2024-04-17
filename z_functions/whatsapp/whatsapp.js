4762adb963ca7ed793391f7f8282aca46ea33958
mariosebastian <mariosebastian@owleyewear.id>
2024-04-17 11:49:54 +0700

    - fixed whatsapp new version

--- z_functions/whatsapp/whatsapp.js
+++ z_functions/whatsapp/whatsapp.js
@@ -15,7 +15,7 @@ mongoose
     client = new Client({
       webVersionCache: {
          type: "remote",
-         remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2403.2.html",
+         remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
       },
       puppeteer: {
         executablePath,
